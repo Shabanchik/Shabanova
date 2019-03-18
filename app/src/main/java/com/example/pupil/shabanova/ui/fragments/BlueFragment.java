278f80936ec941e7c91f0783cb4ba88e5e3393e9
Shabanchik <anastasiyash2003@gmail.com>
2019-03-18 15:41:35 +0200

    comment on tifany color

--- app/src/main/java/com/example/pupil/shabanova/ui/fragments/BlueFragment.java
+++ app/src/main/java/com/example/pupil/shabanova/ui/fragments/BlueFragment.java
@@ -1,5 +1,7 @@
 package com.example.pupil.shabanova.ui.fragments;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,6 +10,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.pupil.shabanova.R;
 import com.example.pupil.shabanova.ui.base.BaseFragment;
@@ -15,23 +19,47 @@ import com.example.pupil.shabanova.ui.base.BaseFragment;
 public class BlueFragment extends BaseFragment {
 
     private BlueViewModel mViewModel;
+    Button btn;
+
 
     public static BlueFragment newInstance() {
         return new BlueFragment();
     }
 
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.blue_fragment, container, false);
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(BlueViewModel.class);
-        // TODO: Use the ViewModel
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(BlueViewModel.class);
+        btn=getView().findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.setIsActive(true);
+            }
+        });
+        mViewModel.isActive.observe(this,new Observer<Boolean>() {
+
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                Toast.makeText(getContext(), "isActive = " + String.valueOf(aBoolean), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+}
 
     @Override
     public String getName() {
--- app/src/main/java/com/example/pupil/shabanova/ui/fragments/BlueViewModel.java
+++ app/src/main/java/com/example/pupil/shabanova/ui/fragments/BlueViewModel.java
@@ -1,7 +1,26 @@
 package com.example.pupil.shabanova.ui.fragments;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.os.CountDownTimer;
 
 public class BlueViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    MutableLiveData<Boolean> isActive= new MutableLiveData<>();
+    void setIsActive(final Boolean isAct){
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                isActive.postValue(isAct);
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    public void isActive(boolean b) {
+    }
 }
